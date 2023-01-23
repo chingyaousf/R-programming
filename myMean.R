5e7bf0a1f05cbce3194cc4ecbcf62fb7987fe191
chingyaousf <96981462+chingyaousf@users.noreply.github.com>
2023-01-22 22:09:52 -0500

    Add files via upload

---
+++ myMean.R
@@ -0,0 +1,23 @@
+#My code
+assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+
+myMean <- function(assignment2) {
+S = sum(assignment2)
+L = length(assignment2)
+M = S/L
+S = c("Sum is:", S)
+L = c("Length is:", L)
+M = c("Mean is:", M)
+print(S, quote = FALSE)
+print(L, quote = FALSE)
+print(M, quote = FALSE)
+}
+myMean (assignment2)
+
+#instructor code
+assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+myMean <- function(assignment2) { return(sum(assignment2)/length(assignment2)) }
+myMean (assignment2)
+
+
+
